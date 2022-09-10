43f897ee55776be370d8b5bf4df123e116f14da3
overplex-eth <112352174+overplex-eth@users.noreply.github.com>
2022-09-10 15:37:20 +0200

    Update simpletoken.sol

--- Token/Erc20/simpletoken.sol
+++ Token/Erc20/simpletoken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0 #never use new versions
+pragma solidity ^0.8.0 //never use new versions
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol'
 
